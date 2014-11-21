8d4e5cb39c9865fe2820e97a0400799fc46e3280
wbroman <william.k.broman@gmail.com>
2014-11-21 11:59:17 -0500

    Removed White Space
    Updated files on live site to remove all white space except for in the
    head of the document.  All "working" pages will be edited and saved with
    a _dev.html suffix to test.  All pages on the live site will have the
    _dev.html suffix removed.

---
+++ head.php
@@ -0,0 +1,16 @@
+  <meta charset="utf-8">
+  <title>TransNav, Inc.</title>
+  <meta name="description" content="CloudSpace - Responsive Hosting HTML Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <!--[if IE]>
+  <script src="js/html5shiv.js"></script>
+  <![endif]-->
+  <!--////////// CSS Stylesheet  ///////////-->
+  <link rel="stylesheet" href="css/style.css">
+  <!--////////// jQuery Source  ///////////-->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+  <!--////////// Layerslider files  ///////////-->
+  <script src="layerslider/js/greensock.js" type="text/javascript"></script>
+  <script src="layerslider/js/layerslider.transitions.js" type="text/javascript"></script>
+  <script src="layerslider/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script> 
---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="ie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <title>TransNav, Inc.</title>
+  <meta name="description" content="CloudSpace - Responsive Hosting HTML Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <!--[if IE]>
+  <script src="js/html5shiv.js"></script>
+  <![endif]-->
+  <!--////////// CSS Stylesheet  ///////////-->
+  <link rel="stylesheet" href="css/style.css">
+  <!--////////// jQuery Source  ///////////-->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+  <!--////////// Layerslider files  ///////////-->
+  <script src="layerslider/js/greensock.js" type="text/javascript"></script>
+  <script src="layerslider/js/layerslider.transitions.js" type="text/javascript"></script>
+  <script src="layerslider/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script> 
+   
+</head>
+<body id="section-home">
+<div class="doc-loader"></div>
+  <!--/////////// HEADER SECTION //////////-->
+  <header>
+    <nav role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <i class="fa fa-th-list iconmobile"></i>
+          </button>
+          <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="logo"></a>
+        </div>
+        <div id="main-nav" class="collapse navbar-collapse navbar-collapse">          
+          <ul class="nav navbar-nav navbar-right">
+            <!--////////// NAVIGATION MENU //////////-->
+            <li><a href="about.html"><span>About</span></a></li>
+            <!-- <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Hosting</a>
+          <ul class="dropdown-menu">
+            <li><a href="plans-1.html"><span>Pricing Table 1</span></a></li>
+            <li><a href="plans-2.html"><span>Pricing Table 2</span></a></li>
+            <li><a href="faq.html"><span>FAQ</span></a></li>
+          </ul>
+        </li> -->
+            <li><a href="production_solutions.html"><span>Production Services</span></a></li>
+            <li><a href="engineering_solutions.html"><span>Engineering Services</span></a></li>
+            <li><a href="innovation.html"><span>Innovation</span></a></li>             
+            <li><a href="contact.html"><span>Contact</span></a></li>
+            
+          </ul>
+        </div><!-- /navbar-collapse -->
+      </div><!-- /container -->
+    </nav>
+  </header>
